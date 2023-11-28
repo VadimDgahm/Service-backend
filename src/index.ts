455e7f97678627d557b013232f9913bac65f5492
VadimDgahm <dgahm@mail.ru>
2023-11-28 09:41:59 +0300

    change index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
-import express,{Request, Response} from 'express'
-const app = express();
-const port = process.env.PORT || 3000;
+import express, { Request, Response } from 'express'
+import { productsRouter } from './routers/products-router';
+import { runDb } from './repositories/db';
 
-// Маршрут для главной страницы
+const app = express()
+const port = process.env.PORT || 5000
+
+const parserMiddleware = express.json()
+app.use(parserMiddleware)
+
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Привет, мир,vbh');
-});
+  res.send('Hello Samurai')
+})
 
+app.use('/products', productsRouter)
 
-// Слушаем порт и запускаем сервер
-app.listen(port, () => {
-  console.log(`Сервер запущен на пор ${port}`);
-});
+const startApp = async () => {
+  await runDb()
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+}
+
+startApp()
